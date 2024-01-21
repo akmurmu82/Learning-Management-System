8c46afce88e2cfbe9a777385f037648d5a195c10
maahin2005 <mahin.malek2005@gmail.com>
2024-01-21 23:10:50 +0530

    Fix the bug of Routes

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,3 +1,28 @@
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import PrivateRoute from "./PrivateRoutes";
+// import Home from "../Pages/Home";
+
+// function AllRoutes() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/admin" element={<h1>ADMIN PAGE</h1>} />
+//       <Route path="/login" element={<h1>LOGIN PAGE</h1>} />
+//       <Route
+//         path="/cart"
+//         element={
+//           <PrivateRoute isAuthenticated={true} element={<h1>CART PAGE</h1>} />
+//         }
+//       />
+//       {/* incorrect route */}
+//       <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+//     </Routes>
+//   );
+// }
+
+// export default AllRoutes;
+
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoutes";
