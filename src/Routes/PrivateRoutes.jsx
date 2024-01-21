72fd76f25fb32a3321140ab61d8933c147f4d965
Amit Kumar <amitkumar655921@gmail.com>
2024-01-21 16:06:16 +0530

    Added routes

---
+++ src/Routes/PrivateRoutes.jsx
@@ -0,0 +1,9 @@
+// PrivateRoutes.js
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
