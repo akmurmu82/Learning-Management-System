117986fa08ac947d99244b01ed79afcbc5e87328
maahin2005 <mahin.malek2005@gmail.com>
2024-01-24 12:51:57 +0530

    Navbar simple design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AllRoutes from "./Routes/AllRoutes";
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AllRoutes />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,255 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  Link,
+  Form,
+  Button,
+  CardBody,
+  ButtonGroup,
+  Heading,
+  Spacer,
+  Stack,
+  Card,
+  HStack,
+} from "@chakra-ui/react";
+import {
+  Portal,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+} from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from "@chakra-ui/react";
+
+export default function Navbar() {
+  // const initialFocusRef = React.useRef();
+
+  const SetInputLabel = ({ inputLabel }) => {
+    return (
+      <>
+        <FormLabel size="sm" fontWeight={500}>
+          {inputLabel}
+        </FormLabel>
+      </>
+    );
+  };
+
+  const SetLastGreenBTN = ({ lastGreenBTN }) => {
+    return (
+      <>
+        <Button
+          borderRadius={20}
+          bg="#2da44e"
+          color="white"
+          size="sm"
+          h="40px"
+          mt="10px"
+          _hover={{ bg: "#2c974b" }}
+          _active={{ bg: "#298e46" }}
+        >
+          {lastGreenBTN}
+        </Button>
+      </>
+    );
+  };
+
+  const GeneralInputBox = ({ type }) => {
+    return (
+      <>
+        <Input
+          type={type}
+          bg="white"
+          bordercolor="#d8dee4"
+          size="sm"
+          w="full"
+          h="40px"
+          mt="5px"
+          outlineColor="#4A55A2"
+          borderRadius={20}
+          color="#4A55A2"
+          p="4"
+          fontWeight={600}
+        />
+      </>
+    );
+  };
+
+  const LoginPopup = () => {
+    return (
+      <>
+        <Popover>
+          <PopoverTrigger>
+            <Button
+              bg="#4385db"
+              color="white"
+              mt="10px"
+              _hover={{ bg: "#3652AD" }}
+              _active={{ bg: "#3468C0" }}
+            >
+              Login
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            px={10}
+            py={5}
+            color="black"
+            bg="#d4d4fd"
+            w="max-content"
+            h="350px"
+            m="auto"
+            borderRadius={5}
+          >
+            <PopoverHeader fontWeight="bold" fontSize="lg">
+              Login Popup
+            </PopoverHeader>
+            <PopoverBody margin="auto">
+              <Card
+                bg="#d4d4fd"
+                variant="outline"
+                bordercolor="#d8dee4"
+                w="300px"
+              >
+                <CardBody>
+                  <form>
+                    <Stack>
+                      <FormControl>
+                        <SetInputLabel inputLabel="Username or email address" />
+
+                        <GeneralInputBox type="text" />
+                      </FormControl>
+                      <FormControl>
+                        <HStack display="flex" justifyContent="space-between">
+                          <SetInputLabel inputLabel="Password" />
+                          <Button
+                            as="a"
+                            href="/"
+                            variant="link"
+                            size="xs"
+                            color="#0969da"
+                            fontWeight={500}
+                          >
+                            Forget Password?
+                          </Button>
+                        </HStack>
+                        <GeneralInputBox type="password" />
+                      </FormControl>
+                      <SetLastGreenBTN lastGreenBTN="Login" />
+                    </Stack>
+                  </form>
+                </CardBody>
+              </Card>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </>
+    );
+  };
+  const SignUpPopup = () => {
+    return (
+      <>
+        <Popover>
+          <PopoverTrigger>
+            <Button
+              bg="#4385db"
+              color="white"
+              mt="10px"
+              _hover={{ bg: "#3652AD" }}
+              _active={{ bg: "#3468C0" }}
+            >
+              SignUp
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            px={10}
+            py={5}
+            color="black"
+            bg="#d4d4fd"
+            w="max-content"
+            h="350px"
+            m="auto"
+            borderRadius={5}
+          >
+            <PopoverHeader fontWeight="bold" fontSize="lg">
+              SignUp Popup
+            </PopoverHeader>
+            <PopoverBody margin="auto">
+              <Card
+                bg="#d4d4fd"
+                variant="outline"
+                bordercolor="#d8dee4"
+                w="300px"
+              >
+                <CardBody>
+                  <form>
+                    <Stack>
+                      <FormControl>
+                        <SetInputLabel inputLabel="Email Address" />
+
+                        <GeneralInputBox type="email" />
+                      </FormControl>
+                      <FormControl>
+                        <SetInputLabel inputLabel="Full Name" />
+
+                        <GeneralInputBox type="text" />
+                      </FormControl>
+                      <FormControl>
+                        <SetInputLabel inputLabel="Set Password" />
+
+                        <GeneralInputBox type="password" />
+                      </FormControl>
+                      <SetLastGreenBTN lastGreenBTN="SignUp" />
+                    </Stack>
+                  </form>
+                </CardBody>
+              </Card>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="#f7f7fe"
+        w="full"
+        h="70px"
+        position="fixed"
+        top="0"
+        left="0"
+        zIndex="999"
+        padding="1rem"
+        px={20}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box>
+          <Heading size="md">
+            <Text fontSize="3xl" fontWeight="bold">
+              Your Logo
+            </Text>
+          </Heading>
+        </Box>
+        <Box display={"flex"} gap={3}>
+          <LoginPopup />
+          <SignUpPopup />
+        </Box>
+      </Flex>
+    </>
+  );
+}
